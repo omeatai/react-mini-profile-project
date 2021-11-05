8cd70752ddb82aef4676c595fe0af741e974d2f7
omeatai <omeatai@gmail.com>
2021-11-05 21:40:15 +0100

    1 file modified

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+const styles = {
+  color: '#fff',
+  backgroundColor: '#000',
+  fontSize: '20px',
+  padding: '50px',
+  textAlign: 'center',
+  fontWeight: 'bold',
+};
+
 function MyHeader() {
-  return <header className="navbar">WELCOME!</header>;
+  return (
+    <header style={styles} className="navbar">
+      WELCOME!
+    </header>
+  );
 }
 
 export default MyHeader;
