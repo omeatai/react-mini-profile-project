dbc53eefab9a53721eda7f5b86be7ba6d6c61f54
omeatai <omeatai@gmail.com>
2021-11-05 08:52:56 +0100

    1 file modified, 1 added

---
+++ components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Ifeanyi Omeata</h1>
+      <p>I am a Software Developer and Cloud Engineer.</p>
+      <p>My top 3 front-end tools are:</p>
+      <ol>
+        <li>React</li>
+        <li>Angularjs</li>
+        <li>Vuejs</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyInfo;
--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Bob Ziroll</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
-  );
-}
+import MyInfo from './components/MyInfo';
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
