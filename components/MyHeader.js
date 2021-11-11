27b87c8bd391d1205372e5c38a18a43b7eb945fd
omeatai <omeatai@gmail.com>
2021-11-11 12:15:26 +0100

    1 file modified

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const myHeaderStyle = {
+  backgroundColor: '#000',
+  color: '#fff',
+  fontWeight: 'bold',
+  padding: '10px',
+};
+
 function MyHeader() {
   return (
     <header>
