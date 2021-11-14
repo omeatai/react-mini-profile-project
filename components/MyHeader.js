889e32a1d6ccaa55a4f61dfef9bf2212191544fc
omeatai <omeatai@gmail.com>
2021-11-14 18:01:11 +0100

    1 file modified

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -8,10 +8,26 @@ const myHeaderStyle = {
   textAlign: 'center',
 };
 
+let timeOfTheDay = 'Morning';
+const date = new Date();
+const hours = date.getHours();
+
+if (hours < 12) {
+  timeOfTheDay = 'Morning';
+  myHeaderStyle.color = 'lightyellow';
+} else if (hours < 17) {
+  timeOfTheDay = 'Afternoon';
+  myHeaderStyle.color = 'red';
+} else {
+  timeOfTheDay = 'Evening';
+  myHeaderStyle.color = 'lightblue';
+}
+console.log(hours);
+
 function MyHeader() {
   return (
     <header style={myHeaderStyle}>
-      <p>Good Morning!</p>
+      <p>Good {timeOfTheDay}!</p>
       <p>WELCOME TO MY PAGE</p>
     </header>
   );
