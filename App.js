997b10b51507ca716c989fb4ae5e419a1e3ed9fa
omeatai <omeatai@gmail.com>
2021-11-17 18:38:50 +0100

    2 files modified

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import MyHeader from './components/MyHeader';
 import Main from './components/Main';
 import MyFooter from './components/MyFooter';
+import './style.css';
 
 function App() {
   return (
