27631fab0224cc20c412ae05b0b4808847ea5025
omeatai <omeatai@gmail.com>
2021-11-17 17:50:07 +0100

    1 file modified

--- components/MyFooter.js
+++ components/MyFooter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function MyFooter() {
-  return <p>Copyright &copy; 2021. Ifeanyi Omeata.</p>;
+  return <small>Copyright &copy; 2021. Ifeanyi Omeata.</small>;
 }
 
 export default MyFooter;
