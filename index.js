3cd4aa26073067e0b2369d55863f2e8d08e35d6d
omeatai <omeatai@gmail.com>
2021-11-17 21:01:04 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyHeader() {
-  return (
-    <header className="nav">
-      <p>Good Morning!</p>
-      <p>WELCOME TO MY PAGE</p>
-    </header>
-  );
-}
-
-function Main() {
-  return (
-    <div>
-      <h2>Ifeanyi Omeata</h2>
-      <p>I am a Software Developer and Cloud Engineer.</p>
-      <p>My top 3 front-end tools are:</p>
-      <ol>
-        <li>React</li>
-        <li>Angularjs</li>
-        <li>Vuejs</li>
-      </ol>
-    </div>
-  );
-}
-
-function MyFooter() {
-  return <small>Copyright &copy; 2021. Ifeanyi Omeata.</small>;
-}
-
-function App() {
-  return (
-    <div>
-      <MyHeader />
-      <Main />
-      <MyFooter />
-    </div>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
