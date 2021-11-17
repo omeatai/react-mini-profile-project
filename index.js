a01fc7db1ceaa79660931e64867cecf1bda6c88a
omeatai <omeatai@gmail.com>
2021-11-17 21:38:18 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-import MyHeader from './components/MyHeader';
-import Main from './components/Main';
-import MyFooter from './components/MyFooter';
-
-function App() {
-  return (
-    <div>
-      <MyHeader />
-      <Main />
-      <MyFooter />
-    </div>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
